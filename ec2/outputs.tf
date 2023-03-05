73074ab835195416b5875d2314ee7ad0950729f3
dewble <dewble62@gmail.com>
2023-03-05 22:35:53 +0900

    Fix initial-install script

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,17 +1,3 @@
-# output "aws_accounts" {
-#   value = yamldecode(file(var.config_file)).aws
-# }
-
-# output "backend" {
-#   value = yamldecode(file(var.config_file)).remote_states.cloud
-# }
-
-
-# output "sg-tags" {
-#   value = "${yamldecode(file(var.config_file)).tags.common_tags.Project}-sg"
-# }
-
-
 output "public-ip" {
   value = aws_instance.jeff-ubuntu.public_ip
 }
