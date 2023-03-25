8b56f2e478689086802547622a8fe72336a1a54a
Jongmin Han / Jeff <39541657+dewble@users.noreply.github.com>
2023-03-26 00:42:34 +0900

    Update initial-install.sh

--- ec2/scripts/initial-install.sh
+++ ec2/scripts/initial-install.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
-# echo "install nginx from apt-get"
-# sudo apt-get update
-# sudo apt-get install -y nginx
 
-echo "start amazon-ssm-agent"
+echo "Starting amazon-ssm-agent"
 sudo snap start amazon-ssm-agent
 
-echo "Install postgresql"
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+echo "Installing PostgreSQL"
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt update
 sudo apt install -y postgresql-14
 
+echo "Setting hostname"
+sudo hostnamectl set-hostname jeff-instance
 
+echo "Setting locale"
+sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+
+echo "Upgrading Python"
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt install -y python3.10 python3.10-venv
+sudo ln -s /usr/bin/python3.10 /usr/bin/python
+python --version
+
+echo "Setting up virtual environment"
+mkdir -pv /home/ubuntu/projects /home/ubuntu/venvs
+python -m venv /home/ubuntu/venvs/jeff-api
+source /home/ubuntu/venvs/jeff-api/bin/activate
+
+echo "Installing Python packages"
+pip install wheel fastapi "uvicorn[standard]" sqlalchemy alembic "pydantic[email]" "passlib[bcrypt]" python-multipart "python-jose[cryptography]" gunicorn
